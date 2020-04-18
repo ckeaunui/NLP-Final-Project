5df9c8db067f21ba25b5ce617922bd12e417b850
unknown <myleshultgren@gmail.com>
2020-04-18 18:27:24 -0500

    Add modules and remove stopwords & punctuation

--- json_reader.py
+++ json_reader.py
@@ -1,6 +1,29 @@
 import json
 import os
-from nltk import word_tokenize
+import numpy as np
+from nltk import *
+import nltk
+import random
+from nltk.corpus import PlaintextCorpusReader
+from nltk.corpus import stopwords
+
+
+def removeStopWords(wordFile):
+    stopwordList = stopwords.words('english')
+    content = [w for w in wordFile if w.lower() not in stopwordList]
+    return content
+
+
+def removePunctuation(wordFile):
+    content = [w for w in wordFile if w.isalpha()]
+    return content
+
+
+def prepareText(wordFile):
+    wordFile = removeStopWords(wordFile)
+    wordFile = removePunctuation(wordFile)
+    wordFile = np.char.lower(wordFile)
+    return wordFile
 
 
 def add_character_speech(json_file, dictionary):
@@ -34,19 +57,22 @@ for subdir, dirs, files in os.walk('json-scripts'):
 for character in speech_by_character:
     print(character)
 
-#print(speech_by_character['deckard'])
+print("Deckard speech: " + str(speech_by_character['deckard']))
 
 # adding protagonist speech
 good_speech = []
-good_speech.append(speech_by_character['deckard'])
-good_speech.append(speech_by_character['z'])
-good_speech.append(speech_by_character['bourne'])
-good_speech.append(speech_by_character['steve'])
-good_speech.append(speech_by_character['lydia'])
+good_speech.append(prepareText(speech_by_character['deckard']))
+good_speech.append(prepareText(speech_by_character['z']))
+good_speech.append(prepareText(speech_by_character['bourne']))
+good_speech.append(prepareText(speech_by_character['steve']))
+good_speech.append(prepareText(speech_by_character['lydia']))
 
 # adding antagonist speech
 bad_speech = []
-bad_speech.append(speech_by_character['betelgeuse'])
-bad_speech.append(speech_by_character['mandible'])
-bad_speech.append(speech_by_character['roth'])
-bad_speech.append(speech_by_character['batty'])
+bad_speech.append(prepareText(speech_by_character['betelgeuse']))
+bad_speech.append(prepareText(speech_by_character['mandible']))
+bad_speech.append(prepareText(speech_by_character['roth']))
+bad_speech.append(prepareText(speech_by_character['batty']))
+bad_speech.append(prepareText(speech_by_character['conklin']))
+
+print("Deckard speech (stopwords, punctuation removed, lowercase): " + str(good_speech[0]))
